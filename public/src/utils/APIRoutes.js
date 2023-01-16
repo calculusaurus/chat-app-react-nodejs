d78010ca177d0d6dc2942d9f490c030700259b8b
ParanoidAltoid <email@maile.com>
2023-01-16 00:17:39 -0700

    fix wrong port issue

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,7 @@
 // export const host = "http://localhost:5000";
-export const host = window.location.origin;
+// export const host = window.location.origin;
+// remove port number from host and add port 5000
+export const host = window.location.origin.replace(/:\d+/, ":5000");
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
